388fa898274e0764b5bc63631802fb9a4dea538c
sarahhp <43327001+sarahhp@users.noreply.github.com>
2018-12-04 14:36:54 +1100

    Switching wd up one level

--- snecklace/scripts/build_de_novo_assembly.py
+++ snecklace/scripts/build_de_novo_assembly.py
@@ -6,7 +6,8 @@
 __author__ = "Sarah Hazell Pickering (sarah.pickering@anu.edu.au)"
 __date__ = "2018-11-02"
 
-DA_OUTDIR = "de_novo_assembly/"
+OUTPUT_DIR = "output_data/"
+DA_OUTDIR = OUTPUT_DIR + "de_novo_assembly/"
 
 #configfile: "necklace.json"
 DATASET = config["dataset"]
