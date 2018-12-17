dd2796593ad30b4095cf4fb1eafb4829147ac6ad
sarahhp <sarah@jcsmr-shiny.anu.edu.au>
2018-12-17 13:09:35 +1100

    Adding pblat (multithreaded capability) to cluster.py

--- commands.sh
+++ commands.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-cd /home/output_data/workflows
+#cd /home/output_data/workflows
+
+DIR="/home/sarah/projects/talens_mouse/pooled_testis_brain_rspermatids/necklace/tmp"
 
 #pre-trim dryrun
 #nakemake -s trim_subsample.py -d /home --configfile config.json --cores 1 -npr 
 #necklace dryrun
-#snakemake -s snecklace/Snakefile -d /home --configfile config.json --cores 1 --use-conda -npr
+#snakemake -s snecklace/Snakefile -d $DIR --configfile config.json --cores 1 --use-conda -npr
 
 #trim datasets: testes, brain and round spermatids. 
 #In reality the brain and spermatid filenames were changed from the raw files as a 
@@ -13,10 +15,13 @@ cd /home/output_data/workflows
 #snakemake -s trim_subsample.py -d /home --configfile config.json --cores 40 -pr
 
 #necklace test case
-snakemake -s snecklace/Snakefile -d /home --configfile 'test.config.json' -j --use-conda -pr --forceall
-snakemake -s snecklace/Snakefile -d /home --configfile 'test.config.json' -j 8 --use-conda -pr --force count_reads
-snakemake -s snecklace/Snakefile -d /home --configfile 'test.config.json' -j 4 --use-conda -pr --force count_reads
-snakemake -s snecklace/Snakefile -d /home --configfile 'test.config.json' -j 2 --use-conda -pr --force count_reads
+#snakemake -s snecklace/Snakefile -d $DIR --configfile 'test.config.json' -j 28  --use-conda -pr 
+#snakemake -s snecklace/Snakefile -d /home --configfile 'test.config.json' -j 8 --use-conda -pr --force count_reads
+#snakemake -s snecklace/Snakefile -d /home --configfile 'test.config.json' -j 4 --use-conda -pr --force count_reads
+#snakemake -s snecklace/Snakefile -d /home --configfile 'test.config.json' -j 2 --use-conda -pr --force count_reads
+
+#test blat/pblat:
+snakemake -s snecklace/Snakefile -d $DIR --configfile 'test.config.json' -j 28 --use-conda -pr blat_genomeST_denovo
 
 #conda test
 #conda create -f snecklace/envs/lace.yml --name lace
--- snecklace/scripts/cluster.py
+++ snecklace/scripts/cluster.py
@@ -12,7 +12,7 @@
 """
 
 __author__ = "Sarah Hazell Pickering (sarah.pickering@anu.edu.au)"
-__date__ = "2018-10-02"
+__date__ = "2018-12-17"
 
 #include: "build_genome_superT.py"
 #include: "build_de_novo_assembly.py"
@@ -20,17 +20,17 @@ __date__ = "2018-10-02"
 
 TOOLS = "tools"
 OUTPUT_DIR = "output_data/"
-CLUSTER_OUTDIR = OUTPUT_DIR + "clustering/"
+CLUSTER_OUTDIR = "clustering/"
 
 #configfile: "necklace.json"
 DATASET = config["dataset"]
 
 """Files from previous workflows:  """
-make_genome = OUTPUT_DIR + "genome_superT/" + DATASET + "_genome_superT.fa" 
+make_genome = "genome_superT/" + DATASET + "_genome_superT.fa" 
 make_related = expand ("{rel_dir}/{species}/{species}_superT.fa",
                        rel_dir = config["relatedsp_superT_dir"],
                        species = config["relatedsp_names"])
-make_de_novo = OUTPUT_DIR + "de_novo_assembly/" + DATASET + "_de_novo.fa"
+make_de_novo = "de_novo_assembly/" + DATASET + "_de_novo.fa"
 
 rule blat_relST_genomeST:
     version: "3.0"
@@ -46,30 +46,32 @@ rule blat_relST_genomeST:
         "blat {input.related} {input.genome} {params} {output} "
 
 rule blat_relST_denovo:
-    version: "3.0"
+    version: "3.7"
     input:
         related = make_related,
         de_novo = make_de_novo
     params:
          config["params"]["blat_related"]
+    threads: config["max_threads"]-1
     benchmark: CLUSTER_OUTDIR + "08blat_relST_denovo.times.tab"
     output:
         CLUSTER_OUTDIR + "relST_denovo.psl"
     shell:
-        "blat {input.related} {input.de_novo} {params} {output}"
+        "pblat threads={threads} {params} {input.related} {input.de_novo} {output}"
 
 rule blat_genomeST_denovo:
-    version: "3.0"
+    version: "3.7"
     input:
         genome = make_genome,
         de_novo = make_de_novo
     params:
         config["params"]["blat"]
+    threads: config["max_threads"]-1
     benchmark: CLUSTER_OUTDIR + "09blat_gST_denovo.times.tab"
     output:
         CLUSTER_OUTDIR + "genomeST_denovo.psl"
     shell:
-        "blat {input.genome} {input.de_novo} {params} {output}"
+        "pblat threads={threads} {params} {input.genome} {input.de_novo} {output}"
 
 rule genome_ST_names:
     version: "3.0"
