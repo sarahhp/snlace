1247393477439953a7cc575b95f235a4a38f00ad
sarahhp <sarah@JCSMR-043221WD.UDS.anu.edu.au>
2018-12-17 12:28:06 +1100

    Small updates

--- commands.sh
+++ commands.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-#cd /home/output_data/workflows
+cd /home/output_data/workflows
 
 #pre-trim dryrun
 #nakemake -s trim_subsample.py -d /home --configfile config.json --cores 1 -npr 
 #necklace dryrun
-snakemake -s snecklace/Snakefile -d ../ --configfile config.json --cores 1 --use-conda -npr
+#snakemake -s snecklace/Snakefile -d /home --configfile config.json --cores 1 --use-conda -npr
 
 #trim datasets: testes, brain and round spermatids. 
 #In reality the brain and spermatid filenames were changed from the raw files as a 
@@ -13,10 +13,16 @@ snakemake -s snecklace/Snakefile -d ../ --configfile config.json --cores 1 --use
 #snakemake -s trim_subsample.py -d /home --configfile config.json --cores 40 -pr
 
 #necklace test case
-#snakemake -s snecklace/Snakefile -d /home --configfile 'test.config.json' --cores 40 --use-conda -pr
+snakemake -s snecklace/Snakefile -d /home --configfile 'test.config.json' -j --use-conda -pr --forceall
+snakemake -s snecklace/Snakefile -d /home --configfile 'test.config.json' -j 8 --use-conda -pr --force count_reads
+snakemake -s snecklace/Snakefile -d /home --configfile 'test.config.json' -j 4 --use-conda -pr --force count_reads
+snakemake -s snecklace/Snakefile -d /home --configfile 'test.config.json' -j 2 --use-conda -pr --force count_reads
 
 #conda test
 #conda create -f snecklace/envs/lace.yml --name lace
 
 #symlink test
-#ln -s config.json master.config.json 
+#ln -s config.json master.config.json
+
+#test lace independently:
+#nakemake -s snecklace/Snakefile -d /home --configfile 'test.config.json' --cores --use-conda -pr run_lace --force
